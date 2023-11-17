f63f66612fe7d3257ed8fc24e08bb744a16148de
liver2 <124078178+liver2@users.noreply.github.com>
2023-11-17 15:16:19 -0500

    add Tester class, data import for party guests

---
+++ Tester.java
@@ -0,0 +1,47 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList;
+
+public class Tester
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<String> peopleString = new ArrayList<String>();
+		ArrayList<ArrayList<String>> peopleStringSeparated = new ArrayList<ArrayList<String>>();
+			
+		try
+		{
+			File partyGuests = new File("partyguests.txt");
+			Scanner scan = new Scanner(partyGuests);
+			int i = 0;
+			
+			while (scan.hasNextLine())
+			{
+				peopleString.add(scan.nextLine());
+				System.out.println(peopleString.get(i));
+				i++;
+			}
+		}
+		catch (FileNotFoundException e) 
+		{
+			System.out.println("File not found");
+		}
+		
+		for(int i = 0; i < peopleString.size(); i++)
+		{
+			for(int j = 0; j < 5; j++)
+			{
+				peopleStringSeparated.add(new ArrayList<String>());
+			}
+			
+			peopleStringSeparated.get(0).add(peopleString.get(i));
+			String[] arrPeople = peopleString.get(i).split(",",4);
+			peopleStringSeparated.get(1).add(arrPeople[0]);
+			peopleStringSeparated.get(2).add(arrPeople[1]);
+			peopleStringSeparated.get(3).add(arrPeople[2]);
+			peopleStringSeparated.get(4).add(arrPeople[3]);
+		}
+		System.out.println(peopleStringSeparated.get(1).get(1));
+	}
+}
