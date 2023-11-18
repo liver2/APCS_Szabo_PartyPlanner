5635c53d3f396214b55d6b68fe9b0e481319d8d1
liver2 <124078178+liver2@users.noreply.github.com>
2023-11-18 15:36:40 -0500

    format Person.java

--- Person.java
+++ Person.java
@@ -1,57 +1,57 @@
 public class Person 
 {
-  private int id;
-  private String firstName;
-  private String lastName;
-  private int companyNumber;
-  private int tableNumber;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int companyNumber;
+    private int tableNumber;
+  
+    public Person (int initId, String initFirstName, String initLastName, int initCompanyNumber) 
+    {
+      id = initId;
+      firstName = initFirstName;
+      lastName = initLastName;
+      companyNumber = initCompanyNumber;
+      tableNumber = 0;
+    }
 
-  public Person (int initId, String initFirstName, String initLastName, int initCompanyNumber) 
-  {
-    id = initId;
-    firstName = initFirstName;
-    lastName = initLastName;
-    companyNumber = initCompanyNumber;
-    tableNumber = 0;
-  }
+    public int getId()
+    {
+      return id;
+    }
 
-  public int getId()
-  {
-    return id;
-  }
+    public String getFirstName()
+    {
+      return firstName;
+    }
 
-  public String getFirstName()
-  {
-    return firstName;
-  }
+    public String getLastName()
+    {
+      return lastName;
+    }
 
-  public String getLastName()
-  {
-    return lastName;
-  }
+    public int getCompanyNumber()
+    {
+      return companyNumber;
+    }
 
-  public int getCompanyNumber()
-  {
-    return companyNumber;
-  }
+    public int getTableNumber()
+    {
+      return tableNumber;
+    }
 
-  public int getTableNumber()
-  {
-    return tableNumber;
-  }
-
-  public void setTableNumber(int value)
-  {
-    tableNumber = value;
-  }
-
-  public String toString()
-  {
-    String result = "";
-    result += "ID: " + id + "\n";
-    result += "Name: " + firstName + " " + lastName + "\n";
-    result += "Company Number: " + companyNumber + "\n";
-    result += "Table Number: " + tableNumber + "\n";
-    return result;
-  }
+    public void setTableNumber(int value)
+    {
+      tableNumber = value;
+    }
+  
+    public String toString()
+    {
+      String result = "";
+      result += "ID: " + id + "\n";
+      result += "Name: " + firstName + " " + lastName + "\n";
+      result += "Company Number: " + companyNumber + "\n";
+      result += "Table Number: " + tableNumber + "\n";
+      return result;
+    }
 }
